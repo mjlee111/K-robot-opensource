ed97ae34b3c7ea56bd65201e8a6c6ed7fff2fa72
Myeung Jin Lee <menggu1234@naver.com>
2024-10-30 14:36:07 +0900

    feat: timer/stopwatch function fin

--- Timer/include/mainwindow.h
+++ Timer/include/mainwindow.h
@@ -3,9 +3,15 @@
 
 #include "../include/serial.hpp"
 
+#include <QComboBox>
+#include <QDialog>
+#include <QDialogButtonBox>
+#include <QHBoxLayout>
 #include <QMainWindow>
 #include <QMessageBox>
+#include <QSpinBox>
 #include <QTimer>
+#include <QVBoxLayout>
 #include <iostream>
 
 #define SERIAL_BAUDRATE 115200
@@ -25,9 +31,18 @@ public:
   MainWindow(QWidget * parent = nullptr);
   ~MainWindow();
 
+  enum TimerType { timer, stopwatch };
+
 private slots:
   void on_deviceList_currentIndexChanged(int index);
   void on_connectBtn_clicked();
+  void on_deviceResetBtn_clicked();
+  void on_servoInitBtn_clicked();
+  void on_timerSetBtn_clicked();
+  void on_stopwatchBtn_clicked();
+  void on_startBtn_clicked();
+  void timerUpdate();
+  void on_resetBtn_clicked();
 
 private:
   Ui::MainWindow * ui;
@@ -35,10 +50,17 @@ private:
   QTimer * msTimer;
 
   void initializeSerial();
+  void timerSetDialog(int & msec, int & sec, int & min);
 
+  TimerType timerType;
+  bool onCount = false;
   int msec = 0;
   int sec = 0;
   int min = 0;
+
+  int last_msec = 0;
+  int last_sec = 0;
+  int last_min = 0;
 
   QString device_name;
   bool isOpen = false;
--- Timer/src/mainwindow.cpp
+++ Timer/src/mainwindow.cpp
@@ -9,6 +9,10 @@ MainWindow::MainWindow(QWidget * parent)
   setWindowIcon(icon);
 
   initializeSerial();
+  timerSetDialog(msec, sec, min);
+
+  timerType = TimerType::stopwatch;
+  ui->modeLabel->setText("Stopwatch");
 }
 
 MainWindow::~MainWindow()
@@ -23,6 +27,13 @@ void MainWindow::initializeSerial()
 {
   QStringList portNames = serial->getPortNames();
   ui->deviceList->addItems(portNames);
+}
+
+void MainWindow::timerSetDialog(int & msec, int & sec, int & min)
+{
+  ui->minLCD->display(QString::number(min).rightJustified(2, '0'));
+  ui->secLCD->display(QString::number(sec).rightJustified(2, '0'));
+  ui->msecLCD->display(QString::number(msec).rightJustified(3, '0'));
 }
 
 //////////////////// Slots ////////////////////
@@ -40,6 +51,8 @@ void MainWindow::on_connectBtn_clicked()
     ui->deviceList->setEnabled(true);
     ui->connectBtn->setText("Connect");
     ui->connectBtn->setStyleSheet("color: rgb(255, 0, 0);");
+    ui->deviceResetBtn->setEnabled(false);
+    ui->servoInitBtn->setEnabled(false);
 
     isOpen = false;
 
@@ -51,6 +64,8 @@ void MainWindow::on_connectBtn_clicked()
       ui->deviceList->setEnabled(false);
       ui->connectBtn->setText("Disconnect");
       ui->connectBtn->setStyleSheet("color: rgb(0, 255, 0);");
+      ui->deviceResetBtn->setEnabled(true);
+      ui->servoInitBtn->setEnabled(true);
 
       isOpen = true;
 
@@ -61,9 +76,176 @@ void MainWindow::on_connectBtn_clicked()
       ui->deviceList->setEnabled(true);
       ui->connectBtn->setText("Connect");
       ui->connectBtn->setStyleSheet("color: rgb(255, 0, 0);");
+      ui->deviceResetBtn->setEnabled(false);
+      ui->servoInitBtn->setEnabled(false);
 
       isOpen = false;
       initializeSerial();
+    }
+  }
+}
+
+void MainWindow::on_deviceResetBtn_clicked()
+{
+  QByteArray packet;
+  packet.append("##");
+  packet.append("0x02");  // reset_device
+  packet.append("//");
+  serial->writeDevice(packet);
+
+  QThread::sleep(1);
+}
+
+void MainWindow::on_servoInitBtn_clicked()
+{
+  QByteArray packet;
+  packet.append("##");
+  packet.append("0x03");  // servo_init
+  packet.append("//");
+  serial->writeDevice(packet);
+
+  QThread::sleep(1);
+}
+
+void MainWindow::on_timerSetBtn_clicked()
+{
+  QDialog dialog(this);
+  dialog.setWindowTitle("타이머 설정");
+
+  QVBoxLayout * mainLayout = new QVBoxLayout(&dialog);
+  QHBoxLayout * inputLayout = new QHBoxLayout();
+
+  QSpinBox * valueSpinBox = new QSpinBox();
+  valueSpinBox->setRange(0, 999);
+
+  QComboBox * unitComboBox = new QComboBox();
+  unitComboBox->addItems({"밀리초", "초", "분"});
+
+  QDialogButtonBox * buttonBox =
+    new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
+
+  inputLayout->addWidget(valueSpinBox);
+  inputLayout->addWidget(unitComboBox);
+  mainLayout->addLayout(inputLayout);
+  mainLayout->addWidget(buttonBox);
+
+  connect(buttonBox, &QDialogButtonBox::accepted, &dialog, &QDialog::accept);
+  connect(buttonBox, &QDialogButtonBox::rejected, &dialog, &QDialog::reject);
+
+  if (dialog.exec() == QDialog::Accepted) {
+    int value = valueSpinBox->value();
+    int unit = unitComboBox->currentIndex();
+
+    int milliseconds = value;
+    switch (unit) {
+      case 1:
+        milliseconds *= 1000;
+        break;
+      case 2:
+        milliseconds *= 60000;
+        break;
+    }
+
+    min = milliseconds / 60000;
+    sec = (milliseconds % 60000) / 1000;
+    msec = milliseconds % 1000;
+
+    last_min = min;
+    last_sec = sec;
+    last_msec = msec;
+
+    timerType = TimerType::timer;
+    ui->modeLabel->setText("Timer");
+    timerSetDialog(msec, sec, min);
+  }
+}
+
+void MainWindow::on_stopwatchBtn_clicked()
+{
+  timerType = TimerType::stopwatch;
+  ui->modeLabel->setText("Stopwatch");
+
+  msec = 0;
+  sec = 0;
+  min = 0;
+  timerSetDialog(msec, sec, min);
+}
+
+void MainWindow::on_startBtn_clicked()
+{
+  if (onCount) {
+    ui->startBtn->setText("Start");
+    msTimer->stop();
+    disconnect(msTimer, &QTimer::timeout, this, &MainWindow::timerUpdate);
+    onCount = false;
+  } else {
+    ui->startBtn->setText("Stop");
+    onCount = true;
+    msTimer->start(1);
+    connect(msTimer, &QTimer::timeout, this, &MainWindow::timerUpdate);
+  }
+}
+
+void MainWindow::timerUpdate()
+{
+  if (timerType == TimerType::timer) {
+    msec--;
+    if (msec < 0) {
+      msec = 999;
+      sec--;
+      if (sec < 0) {
+        sec = 59;
+        min--;
+        if (min < 0) {
+          msec = 0;
+          sec = 0;
+          min = 0;
+          ui->startBtn->setText("Start");
+          msTimer->stop();
+          disconnect(msTimer, &QTimer::timeout, this, &MainWindow::timerUpdate);
+          onCount = false;
+          QMessageBox::information(this, "타이머 종료", "설정한 시간이 완료되었습니다!");
+        }
+      }
+    }
+    timerSetDialog(msec, sec, min);
+  } else if (timerType == TimerType::stopwatch) {
+    msec++;
+    if (msec >= 1000) {
+      msec = 0;
+      sec++;
+      if (sec >= 60) {
+        sec = 0;
+        min++;
+      }
+    }
+    timerSetDialog(msec, sec, min);
+  }
+}
+
+void MainWindow::on_resetBtn_clicked()
+{
+  ui->startBtn->setText("Start");
+  msTimer->stop();
+  disconnect(msTimer, &QTimer::timeout, this, &MainWindow::timerUpdate);
+  onCount = false;
+
+  msec = 0;
+  sec = 0;
+  min = 0;
+
+  timerSetDialog(msec, sec, min);
+
+  if (timerType == TimerType::timer) {
+    QMessageBox::StandardButton reply = QMessageBox::question(
+      this, "타이머 리셋", "마지막으로 설정한 시간으로 복원하시겠습니까?",
+      QMessageBox::Yes | QMessageBox::No);
+
+    if (reply == QMessageBox::Yes) {
+      min = last_min;
+      sec = last_sec;
+      msec = last_msec;
+      timerSetDialog(msec, sec, min);
     }
   }
 }
