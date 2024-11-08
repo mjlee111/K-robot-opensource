c6905f319ed665fc75bcc1fc5ac72126da9d64fe
Myeung Jin Lee <menggu1234@naver.com>
2024-11-09 04:40:12 +0900

    feat: add serialRead()

--- Arduino_firmware/K-Robot/SerialManager.h
+++ Arduino_firmware/K-Robot/SerialManager.h
@@ -3,9 +3,9 @@
 bool initializeSerial();
 String generatePacket(String command);
 void sendPacket(String command);
+String readPacket();
 
-bool initializeSerial()
-{
+bool initializeSerial() {
   Serial.begin(115200);
   while (!Serial) {
   }
@@ -17,8 +17,7 @@ bool initializeSerial()
   return true;
 }
 
-String generatePacket(String command)
-{
+String generatePacket(String command) {
   String packet;
   packet += "##";
   packet += command;
@@ -26,9 +25,25 @@ String generatePacket(String command)
   return packet;
 }
 
-void sendPacket(String command)
-{
+void sendPacket(String command) {
   String packet;
   packet = generatePacket(command);
   Serial.println(packet);
+}
+
+String readPacket() {
+  String packet = "";
+  while (Serial.available() > 0) {
+    char ch = Serial.read();
+    packet += ch;
+
+    if (packet.startsWith("##") && packet.endsWith("FF")) {
+      return packet;
+    }
+
+    if (packet.length() > 64) {
+      packet = "";
+    }
+  }
+  return "";
 }
